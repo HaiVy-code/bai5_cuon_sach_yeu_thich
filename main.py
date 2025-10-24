af2f37c52f80e08f303e1d9059d87392744fa454
HaiVy-coder <buiduonghaivy@gmail.com>
2025-10-24 19:51:42 +0700

    bai5

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ cuaso = App(title = "cac cuon sach yeu thich", width = 370, height = 550, layout
 pic1 = Picture(cuaso, image = "pic1.png", grid = [0,0,1,2], width = 180, height = 270)
 pic2 = Picture(cuaso, image = "pic2.png", grid = [1,0,1,1], width = 180, height = 270)
 pic3 = Picture(cuaso, image = "pic3.png", grid = [1,1,1,1], width = 180, height = 270)
+box = Box(cuaso, grid = [0,3,2,1])
+chu = Text(box, text = "tu sach cua Hai Vy")
 cuaso.display()
